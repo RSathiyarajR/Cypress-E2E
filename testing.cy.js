421605fc9c56d38125a8e71cc160362998d1cabf
RSathiyarajR <qcblrsathiyaraj@gmail.com>
2024-08-26 20:36:23 +0530

    installed cypress

---
+++ testing.cy.js
@@ -0,0 +1,16 @@
+describe('Login Test', () => {
+
+    //Open Cypress | Set ".only"
+    it('should login using data from Excel', () => {
+    
+    cy.visit('https://practicetestautomation.com/practice-test-login/')    
+    cy.readExcelData('C:\\Users\\thani\\Cypress\\ExcelUpload.xlsx', 'Sheet1').then((data)=>{
+    const username = data[1][0]    
+    const password = data[1][1]
+    cy.get('input[name="username"]').type(username)
+    cy.get('input[name="password"]').type(password)
+    cy.get('#submit').click()
+    cy.get('.post-title').should('have.text', 'Logged In Successfully')   
+    })
+    })
+    })
